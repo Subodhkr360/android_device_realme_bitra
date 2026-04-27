20e90d739ceb00b2c830e463c4349220e9f99cc5
Subodh kumar <79571881+Subodhkr360@users.noreply.github.com>
2026-04-27 18:05:37 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,3 +34,4 @@ TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
 include vendor/realme/bitra/BoardConfigVendor.mk
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
