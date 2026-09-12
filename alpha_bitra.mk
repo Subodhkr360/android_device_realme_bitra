16690d4de3f21110adae14f8c407957f17ba7fc8
Subodh kumar <79571881+Subodhkr360@users.noreply.github.com>
2026-09-12 12:58:09 +0530

    Update and rename lineage_bitra.mk to alpha_bitra.mk

--- alpha_bitra.mk
+++ alpha_bitra.mk
@@ -13,9 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/bitra/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_bitra
+PRODUCT_NAME := alpha_bitra
 PRODUCT_DEVICE := bitra
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
@@ -24,6 +24,25 @@ TARGET_HAS_UDFPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+ALPHA_MAINTAINER := Subodh
+TARGET_INCLUDE_AXFX := true
+TARGET_INCLUDE_MATLOG := false
+WITH_ADB_INSECURE := false
+# Extras
+TARGET_INCLUDE_SIMPLE_TUNE := true
+TARGET_PREBUILT_BCR := true
+TARGET_BUILD_PACKAGE := 3
+ifeq ($(TARGET_BUILD_PACKAGE),3)
+  # (valid only for GAPPS builds)
+  TARGET_INCLUDE_GOOGLE_COMMS := true
+  TARGET_INCLUDE_PIXEL_LAUNCHER := false
+  TARGET_SUPPORTS_QUICK_TAP := true
+  TARGET_SUPPORTS_CALL_RECORDING := true
+  TARGET_INCLUDE_STOCK_ARCORE := true
+  TARGET_INCLUDE_LIVE_WALLPAPERS := true
+  TARGET_SUPPORTS_GOOGLE_RECORDER := false
+endif
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="RMX3370-user 13 TP1A.220905.001 R.18c4bac_16f5e-1 release-keys" \
